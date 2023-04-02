24b367993598938e8d8489e14593693c081dd728
anxi01 <tjdals2893@naver.com>
2023-04-02 18:17:34 +0900

    게시판 프로젝트 시작

---
+++ board-spring/board-spring/src/main/java/Board/boardspring/BoardSpringApplication.java
@@ -0,0 +1,13 @@
+package Board.boardspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSpringApplication.class, args);
+	}
+
+}
---
+++ board-spring/board-spring/src/test/java/Board/boardspring/BoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Board.boardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
