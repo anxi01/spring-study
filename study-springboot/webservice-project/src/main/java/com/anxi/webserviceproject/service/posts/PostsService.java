c59290a408aa63b6be8a39402c0c5a3c2ddaacf5
anxi01 <tjdals2893@naver.com>
2023-07-24 17:24:24 +0900

    [study] 게시글 페이지 및 CRUD 기능 구현

--- study-springboot/webservice-project/src/main/java/com/anxi/webserviceproject/service/posts/PostsService.java
+++ study-springboot/webservice-project/src/main/java/com/anxi/webserviceproject/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.anxi.webserviceproject.service.posts;
 
 import com.anxi.webserviceproject.domain.posts.Posts;
 import com.anxi.webserviceproject.domain.posts.PostsRepository;
+import com.anxi.webserviceproject.web.dto.PostsListResponseDto;
 import com.anxi.webserviceproject.web.dto.PostsResponseDto;
 import com.anxi.webserviceproject.web.dto.PostsSaveRequestDto;
 import com.anxi.webserviceproject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. Id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 조회 속도 향상(읽기만 가능)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(posts -> new PostsListResponseDto(posts)) // posts(findALlDest()의 리턴) -> PostsListResponseDto로 변환
+                .collect(Collectors.toList());                 // PostsListResponseDto -> list로 변환
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
